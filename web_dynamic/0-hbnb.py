24c4157eb8dffc10c028ff0a4601989ebea7d965
georgeeset <georgeperfect4u@gmail.com>
2023-05-05 16:42:40 +0100

    css not connecting

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -20,17 +20,17 @@ def hbnb():
     """ main page to hbnb! """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
-    st_ct = []
+    state_list = []
     amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
     for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+        state_list.append([state, sorted(state.cities, key=lambda k: k.name)])
 
     return render_template('0-hbnb.html',
-                           states=st_ct,
+                           states=state_list,
                            amenities=amenities,
                            places=places,
                            cache_id=uuid.uuid4())
