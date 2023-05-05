eed07be07f0bce7aff468913000c67f1c44aff9e
georgeeset <georgeperfect4u@gmail.com>
2023-05-05 16:18:21 +0100

    cash only 0-hbnb

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+import uuid
+from models import storage
+from models.state import State
+from models.place import Place
+from models.amenity import Amenity
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Close SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/0-hbnb', strict_slashes=False)
+def hbnb():
+    """ main page to hbnb! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    return render_template('0-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=uuid.uuid4())
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
