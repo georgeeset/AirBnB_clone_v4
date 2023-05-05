4b0c1eac38dca590d1e1813237405c1727c129b5
georgeeset <georgeperfect4u@gmail.com>
2023-05-05 18:00:13 +0100

    select some amenities to be comfortable

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    //wait till dom is fully loaded before script is run
+
+    const amenities_data = {};
+    $('input:checkbox').click(function () {
+      $(this).each(function () {
+        if (this.checked) {
+          amenities_data[$(this).data('id')] = $(this).data('name');
+        } else {
+          delete amenities_data[$(this).data('id')];
+        }
+      });
+      
+      if (Object.values(amenities_data).length > 0) {
+        $('.amenities h4').text(Object.values(amens).join(', '));
+      } else {
+        $('.amenities h4').html('&nbsp');
+      }
+    });
+  });
